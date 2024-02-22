cbd3efd6f335d9a2f92a3dcda42191d2804027c2
Babuli Sahoo <babuli.sahoo4@gmail.com>
2024-02-22 11:19:09 +0530

    Updated Text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1>Home</h1>
+      <h1>Indian IT Industry</h1>
+      <h2>Managers Brave words to employee</h2>
     </header>
   );
 }
